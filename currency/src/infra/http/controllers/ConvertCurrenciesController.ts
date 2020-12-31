278e04379b56f851342b1721d6c1108e8d0cbbc9
Dayvson Sales <dayvson@outlook.com>
2020-12-31 16:35:52 -0300

    Add convert currencies controller
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

---
+++ currency/src/infra/http/controllers/ConvertCurrenciesController.ts
@@ -0,0 +1,33 @@
+import CurrencyConversionServiceProxy from '@services/proxies/CurrencyConversionServiceProxy';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+export default class ConvertCurrenciesController {
+  async index(request: Request, response: Response): Promise<Response> {
+    const { from, to, amount = 1 } = request.query;
+
+    if (!from && !to) {
+      return response.status(400).send({
+        message: 'Bad request. "from" and "to" are required',
+      });
+    }
+
+    if (!Number(amount)) {
+      return response.status(400).send({
+        message: 'Bad request. Amount need to be a number',
+      });
+    }
+
+    const currencyConversionServiceProxy = container.resolve(
+      CurrencyConversionServiceProxy,
+    );
+
+    const resultConversion = await currencyConversionServiceProxy.convert(
+      from as string,
+      to as string,
+      amount as number,
+    );
+
+    return response.json(resultConversion);
+  }
+}
