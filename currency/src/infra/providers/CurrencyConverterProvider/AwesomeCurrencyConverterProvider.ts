9681d318c94b8bf0974fb39ecf8ff83e4030f4a1
Dayvson Sales <dayvson@outlook.com>
2020-12-31 16:31:43 -0300

    Add currency converter provider (awesome api)
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

---
+++ currency/src/infra/providers/CurrencyConverterProvider/AwesomeCurrencyConverterProvider.ts
@@ -0,0 +1,72 @@
+import CurrencyConversionResponse from '@domain/CurrencyConversionResponse';
+import AppError from '@errors/AppError';
+import ICurrencyConverterProvider from '@providers/CurrencyConverterProvider/ICurrencyConverterProvider';
+import IHTTPProvider from '@providers/HTTPProvider/IHTTPProvider';
+import { injectable, inject } from 'tsyringe';
+
+interface CurrenciesResponseAPI {
+  code: string;
+  codein: string;
+  name: string;
+  high: string;
+  low: string;
+  varBid: string;
+  pctChange: string;
+  bid: string;
+  ask: string;
+  timestamp: string;
+  create_date: Date;
+}
+
+const API_URL = 'https://economia.awesomeapi.com.br/json';
+const CURRENCIES_ENDPOINT = '/all';
+
+@injectable()
+class AwesomeCurrencyConverterProvider implements ICurrencyConverterProvider {
+  constructor(
+    @inject('HTTPProvider')
+    private api: IHTTPProvider,
+  ) {}
+
+  async convert(
+    from: string,
+    to: string,
+    amount: number,
+  ): Promise<CurrencyConversionResponse> {
+    const getFromCurrencyInBRL = await this.getCurrencyValueInBRL(from);
+    const getToCurrencyInBRL = await this.getCurrencyValueInBRL(to);
+
+    const fromToConversion = getFromCurrencyInBRL / getToCurrencyInBRL;
+
+    const response = {
+      from,
+      to,
+      bid: fromToConversion,
+      amountFrom: amount,
+      resultTo: fromToConversion * amount,
+      retrieveDate: new Date(),
+    } as CurrencyConversionResponse;
+
+    return response;
+  }
+
+  private async getCurrencyValueInBRL(from: string): Promise<number> {
+    if (from === 'BRL') {
+      return 1;
+    }
+
+    const { data: fromToBRL } = await this.api.get(
+      `${API_URL}${CURRENCIES_ENDPOINT}/${from}-BRL`,
+    );
+
+    const fromToBRLResponse = fromToBRL[from] as CurrenciesResponseAPI;
+
+    if (!Number(fromToBRLResponse.bid)) {
+      throw new AppError(`Can't convert`, 500);
+    }
+
+    return Number(fromToBRLResponse.bid);
+  }
+}
+
+export default AwesomeCurrencyConverterProvider;
