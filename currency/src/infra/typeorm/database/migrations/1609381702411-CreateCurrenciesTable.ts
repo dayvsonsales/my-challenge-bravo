24c1c0d332676c6fd9665f26e4316bf62de48c1b
Dayvson Sales <dayvson@outlook.com>
2021-01-02 18:25:34 -0300

    Add create extension query to avoid initialization errors
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

--- currency/src/infra/typeorm/database/migrations/1609381702411-CreateCurrenciesTable.ts
+++ currency/src/infra/typeorm/database/migrations/1609381702411-CreateCurrenciesTable.ts
@@ -2,6 +2,8 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateCurrenciesTable1609381702411 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     await queryRunner.createTable(
       new Table({
         name: 'currencies',
