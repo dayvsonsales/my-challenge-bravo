82d2d56701aa88f2da0ba5fedbfeabf56b63a265
Dayvson Sales <dayvson@outlook.com>
2021-01-03 15:26:56 -0300

    Fix find by name condition
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

--- currency/src/services/impl/ListCurrencyService.ts
+++ currency/src/services/impl/ListCurrencyService.ts
@@ -15,9 +15,16 @@ class ListCurrencyService implements IListCurrencyService {
     currencyTo: string,
   ): Promise<void | AppError> {
     const exists = await this.currencyRepository.findByName([
-      currencyFrom,
-      currencyTo,
+      currencyFrom.toUpperCase(),
+      currencyTo.toUpperCase(),
     ]);
+
+    if (currencyFrom === currencyTo) {
+      if (exists?.length == 0) {
+        throw new AppError("One or two currencies doesn't exist", 404);
+      }
+      return;
+    }
 
     if (!exists || exists.length < 2) {
       throw new AppError("One or two currencies doesn't exist", 404);
