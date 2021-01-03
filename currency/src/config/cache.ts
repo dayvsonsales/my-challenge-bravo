01506c263745fd0f2959f9c788bc03e68c22f97a
Dayvson Sales <dayvson@outlook.com>
2021-01-03 15:26:40 -0300

    Fix redis
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

--- currency/src/config/cache.ts
+++ currency/src/config/cache.ts
@@ -1,6 +1,6 @@
 export default {
   redis: {
-    host: process.env.REDIS_HOST || 'http://localhost',
+    host: process.env.REDIS_HOST || '127.0.0.1',
     port: Number(process.env.REDIS_PORT) || 6379,
   },
   resetTime: process.env.CACHE_RESET_TIME || 3600,
